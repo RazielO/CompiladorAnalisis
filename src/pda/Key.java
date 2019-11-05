18560017c7fecce23e9a679bda2fc19e6f08e542
RazielO <oraziel_a@hotmail.com>
2019-11-05 07:38:59 -0600

    Removed tags from Key

--- src/pda/Key.java
+++ src/pda/Key.java
@@ -1,16 +1,11 @@
 package pda;
 
-import lexer.Tag;
-
 import java.util.Objects;
 
 public class Key
 {
     private int state;
     private String value, stackValue;
-
-    //TODO: Remove tags
-    Tag tags = new Tag();
 
     Key(int state, String value, String stackValue)
     {
@@ -43,9 +38,6 @@ public class Key
     @Override
     public String toString()
     {
-//        if (!this.value.equals("λ"))
-//            return String.format("q%d, %12s, %5s", this.state, this.tags.get(Integer.parseInt(this.value)), this.stackValue);
-//        else
             return String.format("q%d, %3s, %3s", this.state, this.value, this.stackValue);
     }
 }
