e4dca504765c5dff1569ec0a3e92bb31de159ab2
RazielO <oraziel_a@hotmail.com>
2019-11-27 17:06:38 -0600

    Arithmetic operation type compatibility

--- src/semantic/Semantic.java
+++ src/semantic/Semantic.java
@@ -30,6 +30,7 @@ public class Semantic
     {
         checkForUndefinedIDs();
         checkForMultipleDeclarations();
+        checkForTypeCompatibility();
     }
 
     private void checkForUndefinedIDs()
@@ -43,6 +44,14 @@ public class Semantic
                 if (this.tag.get(token.getTag()).equals("ID") && !isDefined(token.getLexeme()))
                     this.errorStack.push(new Error(301, token.getLine(), token.getLexeme()));
         }
+    }
+
+    private boolean isDefined(String id)
+    {
+        for (Symbol symbol : this.symbolsTable)
+            if (symbol.getId().equals(id) && symbol.getType() != null)
+                return true;
+        return false;
     }
 
     private void checkForMultipleDeclarations()
@@ -64,12 +73,28 @@ public class Semantic
         }
     }
 
-    private boolean isDefined(String id)
+    private void checkForTypeCompatibility()
+    {
+        for (int i = 0; i < this.tokensList.size(); i++)
+        {
+            String tag = this.tag.get(tokensList.get(i).getTag());
+            if (tag.equals("PLUS") || tag.equals("TIMES") || tag.equals("DIVIDE") || tag.equals("MINUS"))
+            {
+                Symbol a = findSymbol(this.tokensList.get(i - 1).getLexeme());
+                Symbol b = findSymbol(this.tokensList.get(i + 1).getLexeme());
+                if (b != null && a != null)
+                    if (a.getType() != null && b.getType() != null && !a.getType().equals(b.getType()))
+                        this.errorStack.push(new Error(303, this.tokensList.get(i).getLine(), String.format("%s and %s", a.getType(), b.getType())));
+            }
+        }
+    }
+
+    private Symbol findSymbol(String id)
     {
         for (Symbol symbol : this.symbolsTable)
-            if (symbol.getId().equals(id) && symbol.getType() != null)
-                return true;
-        return false;
+            if (symbol.getId().equals(id))
+                return symbol;
+        return null;
     }
 
     public Stack<Error> getErrorStack()
